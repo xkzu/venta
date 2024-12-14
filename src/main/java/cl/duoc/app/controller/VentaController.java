35687977ed20fefae1e54941528d30efc2283093
Kzu <alvaro.vega.pavez@gmail.com>
2024-12-14 18:18:03 -0300

    se quita origin en duro

--- src/main/java/cl/duoc/app/controller/VentaController.java
+++ src/main/java/cl/duoc/app/controller/VentaController.java
@@ -7,7 +7,6 @@ import cl.duoc.app.service.VentaService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +17,6 @@ import java.time.LocalDateTime;
 @Log4j2
 @RestController
 @RequestMapping("/venta")
-@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", allowCredentials = "true")
 public class VentaController {
 
     private final VentaService service;
